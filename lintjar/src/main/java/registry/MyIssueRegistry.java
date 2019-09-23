0efa1c683d258c923fa09e72d186baf841d67a95
chengwen <chengwen02@corp.netease.com>
2019-09-23 20:21:27 +0800

    增加json检验工具

--- lintjar/src/main/java/registry/MyIssueRegistry.java
+++ lintjar/src/main/java/registry/MyIssueRegistry.java
@@ -1,7 +1,9 @@
-package com.paincker.lint.core;
+package registry;
 
 import com.android.tools.lint.client.api.IssueRegistry;
 import com.android.tools.lint.detector.api.Issue;
+import com.paincker.lint.core.LogDetector;
+import com.paincker.lint.core.NewThreadDetector;
 
 import java.util.Arrays;
 import java.util.List;
